ecbcb2a0b49602d2ec04827de8fea8daa74f7b94
kamil jarrouge <zackrowland94@gmail.com>
2022-06-16 16:10:17 +0300

    Schedule model implementation

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    protected $guarded=[
+        'id',
+    ];
+
+    public function cam(){
+        return $this->belongsTo(Cam::class, 'cam_id','id');
+    }
 }
