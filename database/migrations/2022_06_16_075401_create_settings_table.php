2eefa960b7ac682e440f6a02be26e314a6be21cf
kamil jarrouge <zackrowland94@gmail.com>
2022-06-16 11:40:34 +0300

    Models and migrations created and settings table implemented

---
+++ database/migrations/2022_06_16_075401_create_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('warning_thresh');
+            $table->integer('suspension_thresh');
+            $table->integer('attendance_pre');
+            $table->integer('attendance_post');
+            $table->integer('attendance_present');
+            $table->boolean('should_send_sms');
+            $table->string('sms_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
