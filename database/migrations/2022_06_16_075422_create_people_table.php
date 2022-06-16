7078950a2907a4d4546c5cdba53cebf9275c5c58
kamil jarrouge <zackrowland94@gmail.com>
2022-06-16 11:46:34 +0300

    people table implemented

--- database/migrations/2022_06_16_075422_create_people_table.php
+++ database/migrations/2022_06_16_075422_create_people_table.php
@@ -15,6 +15,20 @@ return new class extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->id();
+            $table->string('id_number');
+            $table->string('name');
+            $table->boolean('on_campus');
+            $table->boolean('track');
+            $table->boolean('on_blacklist');
+            $table->boolean('recognize');
+            $table->integer('identity');
+            $table->integer('fri');
+            $table->integer('sat');
+            $table->integer('sun');
+            $table->integer('mon');
+            $table->integer('tue');
+            $table->integer('wed');
+            $table->integer('thu');
             $table->timestamps();
         });
     }
