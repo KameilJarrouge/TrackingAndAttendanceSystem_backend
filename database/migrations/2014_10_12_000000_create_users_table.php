59499b76b37a67db8df437377b93ed7da60cffcf
kamil jarrouge <zackrowland94@gmail.com>
2022-06-16 16:03:36 +0300

    minor fixes and typos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username');
-            $table->boolean('isAdmin'); // adming or prof.
+            $table->boolean('isAdmin'); // admin or professor
             $table->integer('person_id')->nullable();
             $table->integer('semester_id')->nullable();
             $table->string('password');
