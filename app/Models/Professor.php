6e7ab6ef40673964fe44d10332a65ea55b3c81b0
kamil jarrouge <zackrowland94@gmail.com>
2022-06-16 14:12:39 +0300

    Professor  model implemented

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Professor extends Model
 {
     use HasFactory;
+
+    protected $table = 'people';
+
+    public function subjects(){
+        return $this->belongsToMany(Subject::class, 'given_subjects','person_id','subject_id');
+    }
+
 }
