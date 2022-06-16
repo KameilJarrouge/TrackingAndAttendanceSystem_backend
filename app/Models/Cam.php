956d83d06df540b66b72eca111640be8ae953ad6
kamil jarrouge <zackrowland94@gmail.com>
2022-06-16 16:06:26 +0300

    Cam model implementation

--- app/Models/Cam.php
+++ app/Models/Cam.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Cam extends Model
 {
     use HasFactory;
+
+    protected $guarded= [
+        'id',
+    ];
+
+    public function givenSubjects(){
+        return $this->hasMany(GivenSubject::class, 'cam_id','id');
+    }
+
+    public function schedule(){
+        return $this->hasMany(Schedule::class, 'cam_id','id');
+    }
+
+    public function log(){
+        return $this->hasMany(Log::class, 'cam_id', 'id');
+    }
+
 }
