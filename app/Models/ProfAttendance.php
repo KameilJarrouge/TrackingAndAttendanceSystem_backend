84c8000f82456ee8deef056081bfbe1a02ac6e29
kamil jarrouge <zackrowland94@gmail.com>
2022-06-16 14:46:07 +0300

    ProfAttendance  model implemented

--- app/Models/ProfAttendance.php
+++ app/Models/ProfAttendance.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ProfAttendance extends Model
 {
     use HasFactory;
+
+    protected $guarded=[
+        'id',
+    ];
+
+    public function givenSubject(){
+        return $this->belongsTo(GivenSubject::class, 'given_subject_id','id');
+    }
 }
