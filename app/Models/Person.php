c6c43d8d75c81e0d0245746686622b712225f946
kamil jarrouge <zackrowland94@gmail.com>
2022-06-16 13:47:18 +0300

    Person model implemented

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     use HasFactory;
+
+    protected $guarded=[
+        'id',
+        'fri',
+        'sat',
+        'sun',
+        'mon',
+        'tue',
+        'wed',
+        'thu'
+    ];
+
+    public function images(){
+        return $this->hasMany(Image::class, 'person_id','id');
+    }
+
+
+
 }
