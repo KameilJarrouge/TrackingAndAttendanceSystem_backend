acc17ef7db1cfe30bde50c6a14b38020931f4593
kamil jarrouge <zackrowland94@gmail.com>
2022-06-16 13:42:25 +0300

    Setting and Semester models implemented

--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Semester extends Model
 {
     use HasFactory;
+
+    protected $guarded=[
+        'id'
+    ];
+
+    public function givenSubjects(){
+        return $this->hasMany(GivenSubject::class,'semester_id','id');
+    }
+
+    public function takenSubjects(){
+        return $this->hasMany(TakenSubject::class,'semester_id','id');
+    }
+
 }
